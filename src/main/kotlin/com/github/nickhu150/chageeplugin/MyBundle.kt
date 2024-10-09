3200651c32ef87bdc6e285c75b4637ea564bcfe4
GitHub Action <action@github.com>
2024-10-09 03:51:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/nickhu150/chageeplugin/MyBundle.kt
+++ src/main/kotlin/com/github/nickhu150/chageeplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nickhu150.chageeplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/nickhu150/chageeplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nickhu150/chageeplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nickhu150.chageeplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
